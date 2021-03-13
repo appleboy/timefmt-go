1e85c5c82a937d1171d9369874a745f263a29a37
Bo-Yi Wu <appleboy.tw@gmail.com>
2021-03-14 07:41:17 +0800

    chore: format

--- format.go
+++ format.go
@@ -14,18 +14,16 @@ func Format(t time.Time, format string) string {
 // You can use this method to reduce allocations.
 func AppendFormat(buf []byte, t time.Time, format string) []byte {
 	year, month, day := t.Date()
-	hour, min, sec := t.Clock()
-	var width, colons int
+	var width int
 	var padding byte
 	var pending string
-	var upper, swap bool
 	for i := 0; i < len(format); i++ {
 		if b := format[i]; b == '%' {
 			if i++; i == len(format) {
 				buf = append(buf, '%')
 				break
 			}
-			b, width, padding, upper, swap = format[i], 0, '0', false, false
+			b, width, padding = format[i], 0, '0'
 		L:
 			switch b {
 			case '-':
@@ -39,302 +37,30 @@ func AppendFormat(buf []byte, t time.Time, format string) []byte {
 				padding = ^paddingMask
 				b = format[i]
 				goto L
-			case '_':
-				if i++; i == len(format) {
-					goto K
-				}
-				padding = ' ' | ^paddingMask
-				b = format[i]
-				goto L
-			case '^':
-				if i++; i == len(format) {
-					goto K
-				}
-				upper = true
-				b = format[i]
-				goto L
-			case '#':
-				if i++; i == len(format) {
-					goto K
-				}
-				swap = true
-				b = format[i]
-				goto L
-			case '0':
-				if i++; i == len(format) {
-					goto K
-				}
-				padding = '0' | ^paddingMask
-				b = format[i]
-				goto L
-			case '1', '2', '3', '4', '5', '6', '7', '8', '9':
-				width = int(b & 0x0F)
-				const maxWidth = 1024
-				for i++; i < len(format); i++ {
-					b = format[i]
-					if b <= '9' && '0' <= b {
-						width = width*10 + int(b&0x0F)
-						if width >= int((^uint(0)>>1)/10) {
-							width = maxWidth
-						}
-					} else {
-						break
-					}
-				}
-				if width > maxWidth {
-					width = maxWidth
-				}
-				if padding == ^paddingMask {
-					padding = ' ' | ^paddingMask
-				}
-				if i == len(format) {
-					goto K
-				}
-				goto L
+
 			case 'Y':
 				if width == 0 {
 					width = 4
 				}
 				buf = appendInt(buf, year, width, padding)
-			case 'y':
-				if width < 2 {
-					width = 2
-				}
-				buf = appendInt(buf, year%100, width, padding)
-			case 'C':
-				if width < 2 {
-					width = 2
-				}
-				buf = appendInt(buf, year/100, width, padding)
-			case 'g':
-				if width < 2 {
-					width = 2
-				}
-				year, _ := t.ISOWeek()
-				buf = appendInt(buf, year%100, width, padding)
-			case 'G':
-				if width == 0 {
-					width = 4
-				}
-				year, _ := t.ISOWeek()
-				buf = appendInt(buf, year, width, padding)
+
 			case 'm':
 				if width < 2 {
 					width = 2
 				}
 				buf = appendInt(buf, int(month), width, padding)
-			case 'B':
-				buf = appendString(buf, longMonthNames[month-1], width, padding, upper, swap)
-			case 'b', 'h':
-				buf = appendString(buf, shortMonthNames[month-1], width, padding, upper, swap)
-			case 'A':
-				buf = appendString(buf, longWeekNames[t.Weekday()], width, padding, upper, swap)
-			case 'a':
-				buf = appendString(buf, shortWeekNames[t.Weekday()], width, padding, upper, swap)
-			case 'w':
-				for ; width > 1; width-- {
-					buf = append(buf, padding&paddingMask)
-				}
-				buf = append(buf, '0'+byte(t.Weekday()))
-			case 'u':
-				w := int(t.Weekday())
-				if w == 0 {
-					w = 7
-				}
-				for ; width > 1; width-- {
-					buf = append(buf, padding&paddingMask)
-				}
-				buf = append(buf, '0'+byte(w))
-			case 'V':
-				if width < 2 {
-					width = 2
-				}
-				_, week := t.ISOWeek()
-				buf = appendInt(buf, week, width, padding)
-			case 'U':
-				if width < 2 {
-					width = 2
-				}
-				week := (t.YearDay() + 6 - int(t.Weekday())) / 7
-				buf = appendInt(buf, week, width, padding)
-			case 'W':
-				if width < 2 {
-					width = 2
-				}
-				week := t.YearDay()
-				if int(t.Weekday()) > 0 {
-					week -= int(t.Weekday()) - 7
-				}
-				week /= 7
-				buf = appendInt(buf, week, width, padding)
-			case 'e':
-				if padding < ^paddingMask {
-					padding = ' '
-				}
-				fallthrough
+
 			case 'd':
 				if width < 2 {
 					width = 2
 				}
 				buf = appendInt(buf, day, width, padding)
-			case 'j':
-				if width < 3 {
-					width = 3
-				}
-				buf = appendInt(buf, t.YearDay(), width, padding)
-			case 'k':
-				if padding < ^paddingMask {
-					padding = ' '
-				}
-				fallthrough
-			case 'H':
-				if width < 2 {
-					width = 2
-				}
-				buf = appendInt(buf, hour, width, padding)
-			case 'l':
-				if width < 2 {
-					width = 2
-				}
-				if padding < ^paddingMask {
-					padding = ' '
-				}
-				h := hour
-				if h > 12 {
-					h -= 12
-				}
-				buf = appendInt(buf, h, width, padding)
-			case 'I':
-				if width < 2 {
-					width = 2
-				}
-				h := hour
-				if h > 12 {
-					h -= 12
-				} else if h == 0 {
-					h = 12
-				}
-				buf = appendInt(buf, h, width, padding)
-			case 'p':
-				if hour < 12 {
-					buf = appendString(buf, "AM", width, padding, upper, swap)
-				} else {
-					buf = appendString(buf, "PM", width, padding, upper, swap)
-				}
-			case 'P':
-				if hour < 12 {
-					buf = appendString(buf, "am", width, padding, upper, swap)
-				} else {
-					buf = appendString(buf, "pm", width, padding, upper, swap)
-				}
-			case 'M':
-				if width < 2 {
-					width = 2
-				}
-				buf = appendInt(buf, min, width, padding)
-			case 'S':
-				if width < 2 {
-					width = 2
-				}
-				buf = appendInt(buf, sec, width, padding)
-			case 's':
-				if padding < ^paddingMask {
-					padding = ' '
-				}
-				buf = appendInt(buf, int(t.Unix()), width, padding)
-			case 'f':
-				if width == 0 {
-					width = 6
-				}
-				buf = appendInt(buf, t.Nanosecond()/1000, width, padding)
-			case 'Z', 'z':
-				name, offset := t.Zone()
-				if b == 'Z' && name != "" {
-					buf = appendString(buf, name, width, padding, upper, swap)
-					break
-				}
-				i := len(buf)
-				if padding != ^paddingMask {
-					for ; width > 1; width-- {
-						buf = append(buf, padding&paddingMask)
-					}
-				}
-				j := len(buf)
-				if offset < 0 {
-					buf = append(buf, '-')
-					offset = -offset
-				} else {
-					buf = append(buf, '+')
-				}
-				k := len(buf)
-				buf = appendInt(buf, offset/3600, 2, padding)
-				if buf[k] == ' ' {
-					buf[k-1], buf[k] = buf[k], buf[k-1]
-				}
-				if k = offset % 3600; colons <= 2 || k != 0 {
-					if colons != 0 {
-						buf = append(buf, ':')
-					}
-					buf = appendInt(buf, k/60, 2, '0')
-					if k %= 60; colons == 2 || colons == 3 && k != 0 {
-						buf = append(buf, ':')
-						buf = appendInt(buf, k, 2, '0')
-					}
-				}
-				colons = 0
-				if i != j {
-					l := len(buf)
-					k = j + 1 - (l - j)
-					if k < i {
-						l = j + 1 + i - k
-						k = i
-					} else {
-						l = j + 1
-					}
-					copy(buf[k:], buf[j:])
-					buf = buf[:l]
-					if padding&paddingMask == '0' {
-						for ; k > i; k-- {
-							buf[k-1], buf[k] = buf[k], buf[k-1]
-						}
-					}
-				}
-			case ':':
-				if pending != "" {
-					buf = append(buf, ':')
-				} else {
-					colons = 1
-				M:
-					for i++; i < len(format); i++ {
-						switch format[i] {
-						case ':':
-							colons++
-						case 'z':
-							if colons > 3 {
-								i++
-								break M
-							}
-							b = 'z'
-							goto L
-						default:
-							break M
-						}
-					}
-					buf = appendLast(buf, format[:i], width, padding)
-					i--
-					colons = 0
-				}
-			case 't':
-				buf = appendString(buf, "\t", width, padding, false, false)
-			case 'n':
-				buf = appendString(buf, "\n", width, padding, false, false)
 			case '%':
 				buf = appendString(buf, "%", width, padding, false, false)
 			default:
 				if pending == "" {
 					var ok bool
 					if pending, ok = compositions[b]; ok {
-						swap = false
 						break
 					}
 					buf = appendLast(buf, format[:i], width-1, padding)
